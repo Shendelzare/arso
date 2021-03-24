ec2b8be02b8ebfe0e4b7c9cba5d035acb1c4f9a8
T701626 <jgonzamu@everis.com>
2021-03-25 00:30:58 +0100

    Correccion final para Xpath, utilizando bookle Marcos

--- com.arso/src/main/java/com/arso/semana5/ConsultaXPath.java
+++ com.arso/src/main/java/com/arso/semana5/ConsultaXPath.java
@@ -1,7 +1,14 @@
 package com.arso.semana5;
 
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
@@ -14,11 +21,11 @@ public class ConsultaXPath {
 	private static final String RUTA_BOOKLE = "src/main/resources/Bookle.xml";
 
 	private static final String EXPRESION_FECHA = "//fecha";
-	private static final String EXPRESION_HORARIO_TURNOS = "//fecha[text() = '2001-01-01']/..//horario";
-	private static final String EXPRESION_EMAIL_RESERVA = "//reserva[@email]";
-	private static final String EXPRESION_FECHA_MES = "//fecha[contains(text(),'-01-')]";
-	private static final String EXPRESION_PRIMER_TURNO = "//fecha[text() = '2001-01-01']/..//horario";
-	private static final String EXPRESION_ULTIMO_TURNO = "//fecha[text() = '2001-01-01']/..//horario";
+	private static final String EXPRESION_HORARIO_TURNOS = "//horario";
+	private static final String EXPRESION_EMAIL_RESERVA = "//email";
+	private static final String EXPRESION_FECHA_MES = "//fecha[contains(text(),'-04-')]";
+	private static final String EXPRESION_PRIMER_TURNO = "//turno[1]";
+	private static final String EXPRESION_ULTIMO_TURNO = "//turno[last()]";
 
 	public static void main(String[] args) {
 
@@ -26,67 +33,38 @@ public class ConsultaXPath {
 		Document documento = DomUtils.construyeAnalizador(RUTA_BOOKLE);
 
 		NodeList fechas = XpathUtils.realizaConsultaXpath(EXPRESION_FECHA, documento);
-		List<String> fechasConsultadas = consultaFechas(fechas);
+		List<String> fechasConsultadas = recuperaListaString(fechas);
 		fechasConsultadas.forEach(c -> System.out.println("Fecha actual: "+c));
 		
 		NodeList horarioTurnos =  XpathUtils.realizaConsultaXpath(EXPRESION_HORARIO_TURNOS, documento);
-		List<String> turnosConsultados = consultaHorarioTurnos(horarioTurnos);
+		List<String> turnosConsultados = recuperaListaString(horarioTurnos);
 		turnosConsultados.forEach(c -> System.out.println("Turnos obtenidos: "+c));
 
 		NodeList reservas =  XpathUtils.realizaConsultaXpath(EXPRESION_EMAIL_RESERVA, documento);
-		List<String> emailsReserva = consultaEmailReserva(reservas);
+		List<String> emailsReserva = recuperaListaString(reservas);
 		emailsReserva.forEach(c -> System.out.println("Email recuperado: "+c));
 
 		NodeList fechasMes =  XpathUtils.realizaConsultaXpath(EXPRESION_FECHA_MES, documento);
-		List<String> fechasMesResultado = consultaFechaMes(fechasMes);
-		fechasMesResultado.forEach(c -> System.out.println("Fecha actual: "+c));
+		List<String> fechasMesResultado = recuperaListaString(fechasMes);
+		fechasMesResultado.forEach(c -> System.out.println("Fechas para el mes 4: "+c));
+		
+		NodeList primerTurno =  XpathUtils.realizaConsultaXpath(EXPRESION_PRIMER_TURNO, documento);
+		System.out.println("Primer turno: "+primerTurno.item(0).getTextContent());
+		
+		NodeList ultimoTurno =  XpathUtils.realizaConsultaXpath(EXPRESION_ULTIMO_TURNO, documento);
+		System.out.println("Ultimo turno:"+ultimoTurno.item(0).getTextContent());
 	}
 	
-	private static List<String> consultaFechaMes(NodeList fechasMes) {
-		List<String> fechas = new ArrayList<String>();
+	
+	private static List<String> recuperaListaString(NodeList nodelist) {
+		List<String> strings = new ArrayList<String>();
 		
-		for (int i = 0; i < fechasMes.getLength(); i++) {
-			fechas.add(fechasMes.item(i).getTextContent());
+		for (int i = 0; i < nodelist.getLength(); i++) {
+			strings.add(nodelist.item(i).getTextContent());
 		}
-		return fechas;
-	}
-	
-	private static List<String> consultaEmailReserva(NodeList reservas) {
-		List<String> emails = new ArrayList<String>();
-		
-		for (int i = 0; i < reservas.getLength(); i++) {
-			emails.add(reservas.item(i).getTextContent());
-		}
-		return emails;
-	}
-	
-	
-	private static List<String> consultaHorarioTurnoReservas(NodeList horarioTurnos) {
-		List<String> turnos = new ArrayList<String>();
-		
-		for (int i = 0; i < horarioTurnos.getLength(); i++) {
-			turnos.add(horarioTurnos.item(i).getTextContent());
-		}
-		return turnos;
-	}
-	
-	
-	private static List<String> consultaHorarioTurnos(NodeList horarioTurnos) {
-		List<String> turnos = new ArrayList<String>();
-		
-		for (int i = 0; i < horarioTurnos.getLength(); i++) {
-			turnos.add(horarioTurnos.item(i).getTextContent());
-		}
-		return turnos;
-	}
-	private static List<String> consultaFechas(NodeList fechas) {
-		List<String> fechasConsultadas = new ArrayList<String>();
-		
-		for (int i = 0; i < fechas.getLength(); i++) {
-			fechasConsultadas.add(fechas.item(i).getTextContent());
-		}
-		return fechasConsultadas;
+		return strings;
 		
 	}
+	
 
 }
